4210af22ca9a33e2056f6abb6e31742de52f06c9
yuanmingyi <millan.yuan@gmail.com>
2015-02-17 16:48:19 +0800

    fix name

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var http = require('http');
+var url = require('url');
+var util = require('util');
+
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello world\n');
+    req.on('data', function (chunk) {
+        res.write('<p>Data:</p>');
+        res.write(util.inspect(chunk));
+    });
+    req.on('end', function () {
+        rstring = util.inspect(req);
+        res.write('<p>Request:</p>');
+        res.end(rstring);
+    })
 }).listen(port);
+
+console.log('HTTP server is listening at port ' + port);
