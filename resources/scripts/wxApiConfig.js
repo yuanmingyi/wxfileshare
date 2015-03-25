a5a03b3020ff9314bdef36ea2971a37595780843
yuanmingyi <millan.yuan@gmail.com>
2015-03-25 06:16:10 -0700

    test wx share 2

--- resources/scripts/wxApiConfig.js
+++ resources/scripts/wxApiConfig.js
@@ -1,33 +1,17 @@
 wx.ready(function () {
     wx.checkJsApi({
-      jsApiList: jsApiList,
-      success: function (res) {
-        alertBox(JSON.stringify(res));
-      }
-    });
-});
-
-wx.error(function () {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            var wxConfig = JSON.parse(xhr.responseText);
-            wx.config({
-                debug: debugWx,
-                appId: wxConfig.appId,
-                timestamp: wxConfig.timestamp,
-                nonceStr: wxConfig.nonceStr,
-                signature: wxConfig.signature,
-                jsApiList: jsApiList
-            });
+        jsApiList: [
+            'closeWindow',
+            'onMenuShareAppMessage',
+            'onMenuShareTimeline',
+            'onMenuShareQQ',
+            'onMenuShareWeibo'
+        ],
+        success: function (res) {
+            alertBox(JSON.stringify(res));
         }
-    };
-    //xhr.open("POST", "<%= wxConfig.updateSignUrl %>", true);
-    xhr.open("POST", "http://wxfileshare.azurewebsites.net/updateConfig", true);
-    xhr.send(location.href);
-});
+    });
 
-window.onload = function () {
     var shareObj = {
         title: 'test', //'<%= strings.UiShareTitle %>',
         desc: 'test', //'<%= strings.UiShareDescription %>',
@@ -52,4 +36,59 @@ window.onload = function () {
     wx.onMenuShareTimeline(shareObj);
     wx.onMenuShareWeibo(shareObj);
     wx.onMenuShareQQ(shareObj);
-};
+});
+
+wx.error(function () {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            var wxConfig = JSON.parse(xhr.responseText);
+            wx.config({
+                debug: debugWx,
+                appId: wxConfig.appId,
+                timestamp: wxConfig.timestamp,
+                nonceStr: wxConfig.nonceStr,
+                signature: wxConfig.signature,
+                jsApiList: [
+                    'checkJsApi',
+                    'onMenuShareTimeline',
+                    'onMenuShareAppMessage',
+                    'onMenuShareQQ',
+                    'onMenuShareWeibo',
+                    'hideMenuItems',
+                    'showMenuItems',
+                    'hideAllNonBaseMenuItem',
+                    'showAllNonBaseMenuItem',
+                    'translateVoice',
+                    'startRecord',
+                    'stopRecord',
+                    'onRecordEnd',
+                    'playVoice',
+                    'pauseVoice',
+                    'stopVoice',
+                    'uploadVoice',
+                    'downloadVoice',
+                    'chooseImage',
+                    'previewImage',
+                    'uploadImage',
+                    'downloadImage',
+                    'getNetworkType',
+                    'openLocation',
+                    'getLocation',
+                    'hideOptionMenu',
+                    'showOptionMenu',
+                    'closeWindow',
+                    'scanQRCode',
+                    'chooseWXPay',
+                    'openProductSpecificView',
+                    'addCard',
+                    'chooseCard',
+                    'openCard'
+                ]
+            });
+        }
+    };
+    //xhr.open("POST", "<%= wxConfig.updateSignUrl %>", true);
+    xhr.open("POST", "http://wxfileshare.azurewebsites.net/updateConfig", true);
+    xhr.send(location.href);
+});
